ba163926d092b0c1947d128f8dc63b64d7a3c50d
William Goldsworthy <william.goldsworthy@tiscali.co.uk>
2019-02-20 15:17:21 +0000

    Backend

---
+++ my-app/backend/data.js
@@ -0,0 +1,18 @@
+// /backend/data.js
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// this will be our data base's data structure 
+const DataSchema = new Schema(
+  {
+    id: Number,
+    firstname: String,
+    lastname: String,
+    nationality: String,
+    age: Number
+  },
+  { timestamps: true }
+);
+
+// export the new Schema so we could modify it using Node.js
+module.exports = mongoose.model("Data", DataSchema);
